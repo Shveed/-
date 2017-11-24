c4d26938111e689f6f813ae5dd86731a5c6340c8
Shvedkov Ivan <32375298+Shveed@users.noreply.github.com>
2017-11-24 17:15:13 +0300

    Update MassiveChanger.c

--- MassiveChanger.c
+++ MassiveChanger.c
@@ -5,10 +5,10 @@ void main()
 {
     setlocale(LC_ALL, "Russian");
 
-    int m, n, i, j, curr, curr1, minimal, i1, j1;
+    int m, n, i, j, curr, curr1, minimal, i1, j1, c, d;
     m = 5, n = 5, minimal = 100; /* m - amount of lines, n - amount of column */
     int mas[5][5] = {{5, 4, 2, 7, 0}, {5, 4, 2, 7, 1}, {5, 4, 2, 7, 2}, {5, 4, 2, 7, 3}, {5, 4, 2, 7, 4}};
-    int mas[4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
+    int lol[4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
     for (i=0; i<5; i++)
         {
         for (j=0; j<5; j++)
@@ -39,6 +39,7 @@ void main()
           printf("%d\t",mas[i][j]);
     printf("\n");
     }
+    printf("\n");
 
 
 /* Cycle for searching minimal and it's coordinates */
@@ -51,20 +52,35 @@ void main()
                 i1 = i;
                 j1 = j;
             }
-
         }
-    for(i=0; i<3; i++)
-    {
-        for(j=0; j<3; j++)
+    printf("%d\n", minimal);
+    printf("\n");
+    printf("%d, %d\n", i1, j1);
+    printf("\n");
+
+    c = 0;
+    d = 0;
+    for(i=0; i<5; i++)
         {
-
+        if(i != i1)
+            {
+            for(j=0; j<5; j++)
+            {
+                if (j != j1)
+                    lol[c][d] = mas[i][j];
+                    d++;
+            }
+            d = 0;
+            c++;
+            }
         }
-        }
+    for (i=0; i<4; i++)
+    {
+    for (j=0; j<4; j++)
+          printf("%d\t",lol[i][j]);
+    printf("\n");
     }
-
-
-
-}
+    }
 
 
 
