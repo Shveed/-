b968cf90137b16f09671d325df213d948e81680a
Shvedkov Ivan <32375298+Shveed@users.noreply.github.com>
2017-12-15 01:54:11 +0300

    Update InFileSearcher.c

--- InFileSearcher.c
+++ InFileSearcher.c
@@ -5,8 +5,8 @@
 
 int main()
 {
-    FILE *file1;
     char file_name[LEN], line[LEN], file_lines[LEN];
+    int k = 0;
 
     printf("Enter file name including .txt\n");
     scanf("%s", file_name);
@@ -14,20 +14,20 @@ int main()
     printf("Now enter line\n");
     scanf("%s", line);
     printf("You are searching for %s", line);
-    while (file1 = fopen(file_name, "r")!= NULL)
+    file_name = "D:\C\\" + file_name;
+    FILE *file1 = fopen(file_name, "r");
+    if (file1 != NULL)
     {
+        while(fgets(file_lines, LEN, file1) != NULL)
+        {
 
-        fgets(file_lines, LEN, file_name);
+        }
     }
     else printf("Unable to open file. Make sure you entered name correctly");
     fclose(file1);
+
+
+
+
     return 0;
-
-
-
-
-
-
-
-
 }
